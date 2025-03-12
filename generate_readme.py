d21a49779d8a3f9144200fb1e23bfd6e37827e6c
Zero <81680666+ZeroStalker3@users.noreply.github.com>
2025-03-12 17:59:05 +0300

    Update generate_readme.py

--- generate_readme.py
+++ generate_readme.py
@@ -1,7 +1,6 @@
 import requests
 
 def fetch_stats():
-    # Fetch the GitHub stats using API
     stats_url = "https://github-readme-stats.vercel.app/api?username=ZeroStalker3&show_icons=true&theme=radical&layout=compact"
     langs_url = "https://github-readme-stats.vercel.app/api/top-langs/?username=ZeroStalker3&layout=compact&theme=radical"
     
@@ -12,11 +11,9 @@ def fetch_stats():
 
 def update_readme(stats, langs):
     try:
-        # Read the README file
         with open("README.md", "r", encoding="utf-8") as file:
             readme = file.readlines()
 
-        # Find the index of the target line
         start = -1
         for i, line in enumerate(readme):
             if "## 📈 **Статистика GitHub**" in line:
@@ -26,11 +23,9 @@ def update_readme(stats, langs):
         if start == -1:
             raise ValueError("Не найдена строка '## 📈 **Статистика GitHub**' в файле README.md.")
 
-        # Update the lines with the new stats and languages images
         readme[start+2] = f'<img src="{langs}" alt="языки программирования" />\n'
         readme[start+4] = f'<img src="{stats}" alt="статистика" />\n'
         
-        # Write the updated content back to the README file
         with open("README.md", "w", encoding="utf-8") as file:
             file.writelines(readme)
     
