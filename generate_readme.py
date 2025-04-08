0e4d8961393b9b5e581140d2745c0f57f13988ae
Zero <81680666+ZeroStalker3@users.noreply.github.com>
2025-04-08 17:24:14 +0300

    Update generate_readme.py

--- generate_readme.py
+++ generate_readme.py
@@ -3,28 +3,21 @@ import requests
 def fetch_stats():
     stats_url = "https://github-readme-stats.vercel.app/api?username=ZeroStalker3&show_icons=true&theme=radical&layout=compact"
     langs_url = "https://github-readme-stats.vercel.app/api/top-langs/?username=ZeroStalker3&layout=compact&theme=radical"
+    return stats_url, langs_url
 
-    stats = requests.get(stats_url).content.decode()
-    langs = requests.get(langs_url).content.decode()
-    
-    return stats, langs
-
-def update_readme(stats, langs):
+def update_readme(stats_url, langs_url):
     try:
         with open("README.md", "r", encoding="utf-8") as file:
             readme = file.readlines()
-
         start = -1
         for i, line in enumerate(readme):
             if "## 📈 **Статистика GitHub**" in line:
                 start = i
                 break
-        
         if start == -1:
             raise ValueError("Не найдена строка '## 📈 **Статистика GitHub**' в файле README.md.")
-
-        readme[start+2] = f'<img src="{langs}" alt="языки программирования" />\n'
-        readme[start+4] = f'<img src="{stats}" alt="статистика" />\n'
+        readme[start+2] = f'  <img src="{langs_url}" alt="языки программирования" />\n'
+        readme[start+4] = f'  <img src="{stats_url}" alt="статистика" />\n'
         
         with open("README.md", "w", encoding="utf-8") as file:
             file.writelines(readme)
@@ -35,5 +28,5 @@ def update_readme(stats, langs):
         print(f"Произошла ошибка: {e}")
 
 if __name__ == "__main__":
-    stats, langs = fetch_stats()
-    update_readme(stats, langs)
+    stats_url, langs_url = fetch_stats()
+    update_readme(stats_url, langs_url)
